b2cb8be348435991804e5ad608ea86e807f5f9d0
MaxVorosh <ma_voroshilov@mail.ru>
2025-05-05 16:46:46 -0400

    Add CLI

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,65 @@
 #include "tape.h"
 #include "externalSort.h"
 
-int main() {
-    std::string filename = "../examples/example";
-    std::string outName = "../examples/out_example";
+void printUsage() {
+    std::cout << "Usage: ./ExternalSort <input file path> <output file path>" << std::endl;
+    std::cout << "Optional flags:" << std::endl;
+    std::cout << "-rw <number> : Timeout for reading/writing number from tape. Default 1" << std::endl;
+    std::cout << "-move <number> : Timeout for moving head for one space. Default 10" << std::endl;
+    std::cout << "-reset <number> : Timeout for moving tape more than one space. Default 100" << std::endl;
+    std::cout << "-type <number> : Type of sort. 1 is slow sort with lower memory usage, 2 is fast sort with more memory usage. Default 2" << std::endl;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3 || argc % 2 == 0) {
+        printUsage();
+        return 0;
+    }
+    std::string inName = argv[1];
+    std::string outName = argv[2];
+    int rw = 1;
+    int move = 10;
+    int reset = 100;
+    int type = 2;
+    for (int i = 3; i < argc; i+=2) {
+        std::string key = argv[i];
+        int val = std::atoi(argv[i + 1]);
+        if (key == "-rw") {
+            rw = val;
+        }
+        else if (key == "-move") {
+            move = val;
+        }
+        else if (key == "-reset") {
+            reset = val;
+        }
+        else if (key == "-type") {
+            type = val;
+        }
+        else {
+            printUsage();
+            return 0;
+        }
+    }
+
     std::string tmpPath1 = "../tmp/1";
     std::string tmpPath2 = "../tmp/2";
     std::string tmpPath3 = "../tmp/3";
-    {   
-        StatTape ft = StatTape(filename, 1, 10, 100);
-        ft.write(1000);
-        ft.write(10);
-        ft.write(-1);
-    }
-    StatTape in = StatTape(filename, 1, 10, 100);
-    StatTape out = StatTape(outName, 1, 10, 100);
-    StatTape tmp1 = StatTape(tmpPath1, 1, 10, 100);
-    StatTape tmp2 = StatTape(tmpPath2, 1, 10, 100);
-    StatTape tmp3 = StatTape(tmpPath3, 1, 10, 100);
-    // std::cout << in.size() << std::endl;
-    // std::cout << out.size() << std::endl;
-    // std::cout << tmp.size() << std::endl;
-    // std::cout << "Start" << std::endl;
+    
+    StatTape in = StatTape(inName, rw, move, reset);
+    StatTape out = StatTape(outName, rw, move, reset);
+    StatTape tmp1 = StatTape(tmpPath1, rw, move, reset);
+    StatTape tmp2 = StatTape(tmpPath2, rw, move, reset);
+    StatTape tmp3 = StatTape(tmpPath3, rw, move, reset);
     std::vector<Tape*> tmpTapes = {&tmp1, &tmp2, &tmp3};
-    FastSorter sorter(10);
-    sorter.sort(&in, &out, tmpTapes);
-    StatTape reader = StatTape(outName, 1, 10, 100);
-    for (int i = 0; i < reader.size(); ++i) {
-        std::cout << reader.read() << ' ';    
+
+    if (type == 1) {
+        SimpleSorter sorter(10);
+        sorter.sort(&in, &out, tmpTapes);
     }
-    std::cout << std::endl;
-    // std::cout << in.size() << std::endl;
-    // std::cout << tmp.size() << std::endl;
-    // std::cout << reader.size() << std::endl;
-    std::cout << in.getTime() + out.getTime() + tmp1.getTime() + tmp2.getTime() + tmp3.getTime() << std::endl;
+    else {
+        FastSorter sorter(10);
+        sorter.sort(&in, &out, tmpTapes);
+    }
+    std::cout << "Time: " << in.getTime() + out.getTime() + tmp1.getTime() + tmp2.getTime() + tmp3.getTime() << std::endl;
 }
